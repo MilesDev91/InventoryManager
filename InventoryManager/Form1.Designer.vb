576e38df4ee708eedfab314cf8ebf2cbd3054bed
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-03-29 10:27:06 -0400

    This app can load an excel form, display it on a grid, and add or change items on the grid. It currently is only functional for a specific type of excel table.

--- InventoryManager/Form1.Designer.vb
+++ InventoryManager/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,169 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.grdExcelTable = New System.Windows.Forms.DataGridView()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        Me.txtFileName = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.lblItem = New System.Windows.Forms.Label()
+        Me.txtItem = New System.Windows.Forms.TextBox()
+        Me.lblQuantity = New System.Windows.Forms.Label()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.lblPrice = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnSaveChanges = New System.Windows.Forms.Button()
+        CType(Me.grdExcelTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grdExcelTable
+        '
+        Me.grdExcelTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdExcelTable.Location = New System.Drawing.Point(403, 12)
+        Me.grdExcelTable.Name = "grdExcelTable"
+        Me.grdExcelTable.Size = New System.Drawing.Size(478, 460)
+        Me.grdExcelTable.TabIndex = 0
+        '
+        'lblFileName
+        '
+        Me.lblFileName.AutoSize = True
+        Me.lblFileName.Location = New System.Drawing.Point(409, 483)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(57, 13)
+        Me.lblFileName.TabIndex = 1
+        Me.lblFileName.Text = "File Name:"
+        '
+        'txtFileName
+        '
+        Me.txtFileName.Location = New System.Drawing.Point(472, 478)
+        Me.txtFileName.Name = "txtFileName"
+        Me.txtFileName.Size = New System.Drawing.Size(328, 20)
+        Me.txtFileName.TabIndex = 2
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(806, 478)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 3
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'lblItem
+        '
+        Me.lblItem.AutoSize = True
+        Me.lblItem.Location = New System.Drawing.Point(12, 12)
+        Me.lblItem.Name = "lblItem"
+        Me.lblItem.Size = New System.Drawing.Size(27, 13)
+        Me.lblItem.TabIndex = 4
+        Me.lblItem.Text = "Item"
+        '
+        'txtItem
+        '
+        Me.txtItem.Location = New System.Drawing.Point(15, 28)
+        Me.txtItem.Name = "txtItem"
+        Me.txtItem.Size = New System.Drawing.Size(271, 20)
+        Me.txtItem.TabIndex = 5
+        '
+        'lblQuantity
+        '
+        Me.lblQuantity.AutoSize = True
+        Me.lblQuantity.Location = New System.Drawing.Point(15, 73)
+        Me.lblQuantity.Name = "lblQuantity"
+        Me.lblQuantity.Size = New System.Drawing.Size(46, 13)
+        Me.lblQuantity.TabIndex = 6
+        Me.lblQuantity.Text = "Quantity"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Location = New System.Drawing.Point(18, 89)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(55, 20)
+        Me.txtQuantity.TabIndex = 7
+        '
+        'lblPrice
+        '
+        Me.lblPrice.AutoSize = True
+        Me.lblPrice.Location = New System.Drawing.Point(90, 73)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(175, 13)
+        Me.lblPrice.TabIndex = 8
+        Me.lblPrice.Text = "Price (Optional if item already exists)"
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(93, 89)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrice.TabIndex = 9
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(18, 141)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 10
+        Me.btnAdd.Text = "Add Item"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Image = Global.InventoryManager.My.Resources.Resources.Trash
+        Me.Button2.Location = New System.Drawing.Point(366, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(31, 24)
+        Me.Button2.TabIndex = 11
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnSaveChanges
+        '
+        Me.btnSaveChanges.Location = New System.Drawing.Point(18, 197)
+        Me.btnSaveChanges.Name = "btnSaveChanges"
+        Me.btnSaveChanges.Size = New System.Drawing.Size(111, 23)
+        Me.btnSaveChanges.TabIndex = 12
+        Me.btnSaveChanges.Text = "Save Changes"
+        Me.btnSaveChanges.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(893, 534)
+        Me.Controls.Add(Me.btnSaveChanges)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.lblQuantity)
+        Me.Controls.Add(Me.txtItem)
+        Me.Controls.Add(Me.lblItem)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtFileName)
+        Me.Controls.Add(Me.lblFileName)
+        Me.Controls.Add(Me.grdExcelTable)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.grdExcelTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents grdExcelTable As DataGridView
+    Friend WithEvents lblFileName As Label
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents lblItem As Label
+    Friend WithEvents txtItem As TextBox
+    Friend WithEvents lblQuantity As Label
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents lblPrice As Label
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnSaveChanges As Button
 End Class
--- InventoryManager/Form1.vb
+++ InventoryManager/Form1.vb
@@ -1,3 +1,100 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports ExcelDataReader
 
+Public Class Form1
+    Dim tables As DataTableCollection
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Using openFileDialog As OpenFileDialog = New OpenFileDialog() With {.Filter = "(*.xlsx;*.xsl)|*.xlsx;*.xls"}
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                txtFileName.Text = openFileDialog.FileName
+                Using stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read)
+                    Using reader As IExcelDataReader = ExcelReaderFactory.CreateReader(stream)
+                        Dim result As DataSet = reader.AsDataSet(New ExcelDataSetConfiguration() With {.ConfigureDataTable = Function(__) New ExcelDataTableConfiguration() With {.UseHeaderRow = True}})
+                        tables = result.Tables
+                        For Each table As DataTable In tables
+                            grdExcelTable.DataSource = table
+                        Next
+                    End Using
+                End Using
+            End If
+        End Using
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If grdExcelTable.RowCount < 1 Then
+            MsgBox("Must have a loaded table")
+            Exit Sub
+        End If
+
+        Dim itemName As String
+        Dim itemPrice As Decimal
+        Dim itemQuantity As Integer
+
+        Try
+            itemName = txtItem.Text
+            itemQuantity = Integer.Parse(txtQuantity.Text)
+            If String.IsNullOrEmpty(txtPrice.Text) = False Then
+                itemPrice = Decimal.Parse(txtPrice.Text)
+            End If
+        Catch ex As Exception
+            MsgBox("Please check form values")
+            Exit Sub
+        End Try
+
+        Dim err As String = ValidateAddItemForm(itemName, itemQuantity, itemPrice)
+        If err <> "" Then
+            MsgBox(err)
+            Exit Sub
+        End If
+
+        'Check rows for existing item
+        For Each row As DataGridViewRow In grdExcelTable.Rows
+            If row.Cells(0).Value IsNot DBNull.Value Then
+                If row.Cells(0).Value = itemName Then
+                    row.Cells(1).Value += itemQuantity
+                    If String.IsNullOrEmpty(txtPrice.Text) = False Then
+                        row.Cells(2).Value = itemPrice
+                    End If
+                    clearAddItemForm()
+                    Exit Sub
+                End If
+            End If
+            If row.Cells(0).Value Is DBNull.Value Then
+                'Price must be set for new items
+                If String.IsNullOrEmpty(txtPrice.Text) Then
+                    MsgBox("You must set a price for new items")
+                    Exit Sub
+                End If
+                row.Cells(0).Value = itemName
+                row.Cells(1).Value = itemQuantity
+                row.Cells(2).Value = itemPrice
+                clearAddItemForm()
+                Exit Sub
+            End If
+        Next
+    End Sub
+
+    Private Function validateAddItemForm(name As String, quantity As Integer, price As Decimal)
+        Dim errorMessage As String = ""
+
+        If name.Length > 50 Or name.Length = 0 Then
+            errorMessage = "Name is too long or too short"
+        End If
+
+        If quantity >= 100000 Then
+            errorMessage = "Please enter a quantity below 100,000"
+        End If
+
+        If price >= 100000 Then
+            errorMessage = "Please enter a price below 100,000"
+        End If
+
+        Return errorMessage
+    End Function
+
+    Private Sub clearAddItemForm()
+        txtItem.Text = ""
+        txtPrice.Text = ""
+        txtQuantity.Text = 0
+    End Sub
 End Class
